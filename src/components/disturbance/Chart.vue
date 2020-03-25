326596e2dc73eba1d30cb0c694eed360f2a59d9d
Amyang <amyang.xyz@gmail.com>
2020-03-25 13:10:58 -0400

    deduplication

--- src/components/disturbance/Chart.vue
+++ src/components/disturbance/Chart.vue
@@ -49,6 +49,7 @@ export default {
 
           markLine: {
             animation: false,
+            silent:true,
             // symbolSize: 8,
             lineStyle:{
               type: 'solid',
@@ -68,7 +69,7 @@ export default {
             opacity:0,
           },
           symbolSize:20,
-          hoverAnimation: false
+          // hoverAnimation: false
         },
         {
           type: 'effectScatter',
@@ -87,7 +88,17 @@ export default {
           },
           symbolSize:30,
           hoverAnimation: false
-        },]
+        },
+        {
+          type: 'scatter',
+          data: [],
+          itemStyle: {
+            color: 'red',
+            opacity:0.4,
+          },
+          symbolSize:30,
+          hoverAnimation: false
+        }]
       }
     }
   },
@@ -95,32 +106,31 @@ export default {
     draw() {    
       this.option.series[0].data = []
       this.option.series[0].markLine.data = []
-      
       // gen invisible node for click event
       for(var a=0;a<=20;a++) {
         for(var b=0;b<=20;b++) {
           this.option.series[1].data.push([a,b])
         }
       }
+
       // gen nodes
       this.nodes = {0:{parent:-1,position:[1,1]}}
-
+      var pos_list = {'1-1':1}
       for(var i=1;i<100;i++) {
         var x=Math.round((18)*Math.random()+1)
         var y=Math.round((18)*Math.random()+1)
+        while(pos_list[x+'-'+y]!=null) {
+          x=Math.round((18)*Math.random()+1)
+          y=Math.round((18)*Math.random()+1)
+        }
+        pos_list[x+'-'+y] = 1
         this.nodes[i]={parent:-1,position:[x,y]}
       }
-      
-      this.findParents()
-    },
-    findParents() {
-      this.option.series[0].data = []
-      this.option.series[0].markLine.data = []
-      // reset
-      for(var c=0;c<Object.keys(this.nodes).length;c++) {
-        this.nodes[c].parent = -1
+      for(var nn=0;nn<Object.keys(this.nodes).length;nn++) {
+        this.option.series[0].data.push(this.nodes[nn].position)
       }
 
+      // find parents
       var layers = {0:[0]}
       var cur_layer = 1
       var cnt = 1
@@ -129,32 +139,24 @@ export default {
         layers[cur_layer] = []
         while(layers[cur_layer].length<1) {
           for(var j=1;j<Object.keys(this.nodes).length;j++) {
-            // has parent
+            // assigned
             if(this.nodes[j].parent!=-1) continue
 
             // distance to possible parents
             var distance_list = []
             for(var p=0;p<layers[cur_layer-1].length;p++) {
               var pp = layers[cur_layer-1][p]
-              var flag = 0
-              // not in blacklist
-              for(var l=0;l<this.blacklist.length;l++) {
-                if(this.blacklist[l]==pp) {
-                  window.console.log(pp)
-                  flag = 1
-                }
-              }
-              if(flag) continue
               var distance = Math.pow(this.nodes[j].position[0]-this.nodes[pp].position[0], 2) + Math.pow(this.nodes[j].position[1]-this.nodes[pp].position[1], 2)
               distance_list.push({id:pp,d:distance})
             }
             var nearest_parent = distance_list.sort((a, b)=>(a.d>b.d)?1:-1)[0]
 
-            if(nearest_parent.d<=threshold) {
+            if(nearest_parent.d<=threshold) {            
               this.nodes[j].parent = nearest_parent.id
               layers[cur_layer].push(j)
               cnt++
-              this.option.series[0].markLine.data.push([{coord:this.nodes[j].position},{coord:this.nodes[nearest_parent.id].position}])
+
+              this.drawLine([{coord:this.nodes[j].position},{coord:this.nodes[nearest_parent.id].position}])
             }
           }
         
@@ -163,11 +165,23 @@ export default {
         }
         cur_layer++
       }
-        
-      for(var nn=0;nn<Object.keys(this.nodes).length;nn++) {
-        this.option.series[0].data.push(this.nodes[nn].position)
-      }
     },
+    changeParent() {
+
+    },
+    drawLine(line) {
+      this.option.series[0].markLine.data.push(line)
+    },
+    // eraseLine(link) {
+    //   for(var i=0;i<this.option.series[0].markLine.data.length;i++) {
+    //     var L = this.option.series[0].markLine.data[i]
+    //     if(link[0].coord==L[0].coord && link[1].coord==L[1].coord) {
+    //       this.option.series[0].markLine.data[i].splice(i,1)
+    //       break
+        
+    //     }
+    //   }
+    // },
     addNoise(param) {
       if(this.noisePos[0]==param.value[0] && this.noisePos[1]==param.value[1]) {
         this.noisePos = []
@@ -175,24 +189,26 @@ export default {
       } else {
         this.noisePos = param.value
         this.option.series[2].data = [param.value]
-        this.respondToNoise()
       }
+      this.respondToNoise()
     },
     respondToNoise() {
       var affected = []
+      this.option.series[4].data = []
       for(var i=0;i<Object.keys(this.nodes).length;i++) {
         var distance = Math.pow(this.nodes[i].position[0]-this.noisePos[0], 2) + Math.pow(this.nodes[i].position[1]-this.noisePos[1], 2)
-        if(distance<=5)
-          affected.push(i)
+        if(distance<=5&&i!=0) {
+          var lv = 3
+          if(distance<=4) lv = 2
+          if(distance<=1) lv = 1
+          affected.push({id:i,lv:lv})
+          this.option.series[4].data.push(this.nodes[i].position)
+        }
       }
+      this.option.series[4].data = Array.from(new Set(this.option.series[4].data))
       this.blacklist = affected
-      window.console.log(this.blacklist)
-      this.findParents()
-      // find new parents
-      // for(var j=0;j<Object.keys(this.nodes).length;j++) {
-      //   affected[j]
-      // }
-
+    
+      this.changeParents()
     }
   },
   mounted() {
