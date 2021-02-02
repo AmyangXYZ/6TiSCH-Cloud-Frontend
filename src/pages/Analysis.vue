de326320a88ee8ab7ad9d367db694a15c9bebc57
AmyangXYZ <amyang.xyz@gmail.com>
2021-02-01 21:35:04 -0500

    clean dir

--- src/pages/Analysis.vue
+++ src/pages/Analysis.vue
@@ -1,68 +1,14 @@
 <template>
   <div>
-    <!-- <vs-row vs-align="flex-start" vs-w="12">
-      <vs-col style="z-index:99" vs-offset="0.8" vs-w="5">  
-        <LatCmp/>
-      </vs-col>
-      <vs-col style="z-index:99" vs-offset="0.3" vs-w="5">  
-        <LatCmpNoise/>
-      </vs-col>
-    </vs-row>
-    <vs-row vs-align="flex-start" style="margin-top:22px" vs-w="12">
-      <vs-col vs-offset="0.8" vs-w="5">  
-        <RTTCmp/>
-      </vs-col>
-      <vs-col vs-offset="0.3" vs-w="5">  
-        <RTTCmpNoise/>
-      </vs-col>
-    </vs-row>
-    <vs-row vs-align="flex-start" style="margin-top:22px" vs-w="12">
-      <vs-col vs-offset="0.8" vs-w="5">  
-        <DSRCmp/>
-      </vs-col>
-      <vs-col vs-offset="0.3" vs-w="5">  
-        <DSRCmpNoise/>
-      </vs-col>
-    </vs-row>
-    <vs-row vs-align="flex-start" style="margin-top:22px" vs-w="12">
-      <vs-col vs-offset="6.1" vs-w="5">  
-        <Noise/>
-      </vs-col>
-    </vs-row>
-    <vs-row vs-align="flex-start" style="margin-top:22px" vs-w="12">
-      <vs-col vs-offset="0.8" vs-w="6.5">  
-        <TestbedLatCmp/>
-      </vs-col>
-      <vs-col vs-offset="0.8" vs-w="6.5">  
-        <TestbedRTTCmp/>
-      </vs-col>
-    </vs-row>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> -->
+    
   </div>
 </template>
 
 <script>
-// import LatCmp from '../components/analysis/LatCmp'
-// import RTTCmp from '../components/analysis/RTTCmp'
-// import DSRCmp from '../components/analysis/DSRCmp'
-// import LatCmpNoise from '../components/analysis/LatCmpNoise'
-// import RTTCmpNoise from '../components/analysis/RTTCmpNoise'
-// import DSRCmpNoise from '../components/analysis/DSRCmpNoise'
-// import Noise from '../components/analysis/Noise'
-// import TestbedLatCmp from '../components/analysis/TestbedLatCmp'
-// import TestbedRTTCmp from '../components/analysis/TestbedRTTCmp'
+
 
 export default {
     components: {
-      // TestbedLatCmp,
-      // TestbedRTTCmp,
-      // LatCmp,
-      // RTTCmp,
-      // DSRCmp,
-      // LatCmpNoise,
-      // RTTCmpNoise,
-      // DSRCmpNoise,
-      // Noise
     }
 }
 </script>
