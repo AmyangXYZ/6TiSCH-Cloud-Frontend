bfb56ac791da098010eb5bf3147f4e124869f34a
AmyangXYZ <amyang.xyz@gmail.com>
2021-10-19 20:16:57 -0400

    dynamic experiment figure

--- src/components/dashboard/LatencyChart2.vue
+++ src/components/dashboard/LatencyChart2.vue
@@ -1,7 +1,7 @@
 <template>
-  <vs-card style="margin-top:22px">
-    <div slot="header"><h4>E2E - Dynamic Experiment </h4></div>
-    <ECharts autoresize :options="option"/>
+  <vs-card style="margin-top: 22px">
+    <div slot="header"><h4>E2E - Dynamic Experiment</h4></div>
+    <ECharts autoresize :options="option" />
   </vs-card>
 </template>
 
@@ -13,11 +13,11 @@ import "echarts/lib/component/title";
 import "echarts/lib/component/dataZoom";
 import "echarts/lib/component/markLine";
 
-import latency from './lat_dynamic.json'
+import latency from "./lat_dynamic.json";
 
 export default {
   components: {
-    ECharts
+    ECharts,
   },
   data() {
     return {
@@ -25,66 +25,132 @@ export default {
       selectedLayer: -1,
       data: [],
       successCnt: 0,
-      totalCnt:0,
-      option:{
-        grid:[{
-          right: "5%",
+      totalCnt: 0,
+      option: {
+        grid: [
+          {
+            right: "5%",
             top: "18%",
-            bottom: "5%",
-            left: "56%",
-            containLabel: true 
-          }
+            bottom: "2%",
+            left: "45%",
+            containLabel: true,
+          },
         ],
         xAxis: {
-          type: 'time',
+          type: "time",
           splitLine: {
-              show: false
-          }
-        },
-        yAxis: {
-          type: 'value',
-          boundaryGap: [0, 0.2],
-          splitLine: {
-              show: false
-          }
-        },
-        series: [{
-          name: '模拟数据',
-          type: 'scatter',
-          symbol: "circle",
-          symbolSize: 8,
-
-          itemStyle: {
-            borderColor: "red",
-            color: "white",
+            show: true,
           },
-          showSymbol: false,
-          hoverAnimation: false,
-          data: []
-        }]
-      }
-    }
+          maxInterval: 6000,
+          name: "time",
+          nameTextStyle: {
+            fontSize: 18,
+            align: "center",
+          },
+          nameLocation: "center",
+          nameGap: 45,
+          axisLabel: {
+            fontSize: 16,
+            rotate: 30,
+            // fontWeight:"bold"
+          },
+        },
+        yAxis: [
+          {
+            name: "E2E Latency (s)",
+            nameTextStyle: {
+              fontSize: 18,
+              // fontWeight: "bold",
+              align: "center",
+            },
+            axisLabel: {
+              fontSize: 16,
+              // fontWeight:"bold"
+            },
+            type: "value",
+            boundaryGap: [0, 0.2],
+            splitLine: {
+              show: true,
+            },
+          },
+          {
+            name: "Task Rate",
+            nameTextStyle: {
+              fontSize: 18,
+              // fontWeight: "bold",
+              align: "center",
+            },
+            interval: 1,
+            axisLabel: {
+              fontSize: 16,
+              // fontWeight:"bold"
+            },
+            type: "value",
+            boundaryGap: [0, 0.2],
+            splitLine: {
+              show: true,
+            },
+          },
+        ],
+        series: [
+          {
+            name: "latency",
+            type: "scatter",
+            symbol: "circle",
+            symbolSize: 12,
+            yAxisIndex:0,
+            itemStyle: {
+              borderWidth: 1.5,
+              borderColor: "red",
+              color: "white",
+            },
+            hoverAnimation: false,
+            data: [],
+          },
+          {
+            name: "rate",
+            type: "line",
+            lineStyle: {
+              color:"black",
+              width: 2.5
+            },
+            showSymbol: false,
+            hoverAnimation: false,
+            data: [],
+            yAxisIndex: 1,
+          },
+        ],
+      },
+    };
   },
   methods: {
     draw() {
-      var data = latency.data
-      for(var i=0;i<data.length;i++) {
+      var data = latency.data;
+      for (var i = 0; i < data.length; i++) {
         // this.option.xAxis.data.push(data[i].timestamp)
-        this.option.series[0].data.push([data[i].timestamp, data[i].e2e_latency])
+        this.option.series[0].data.push([
+          data[i].timestamp,
+          data[i].e2e_latency,
+        ]);
+        var rate = 1  
+        if(i>9&&i<35) rate = 1.5
+        if(i>=35) rate = 3
+        this.option.series[1].data.push([
+          data[i].timestamp,
+          rate,
+        ]);
       }
     },
   },
   mounted() {
-    
-    this.draw()
-  
-
-  }
+    this.draw();
+  },
 };
 </script>
 
 <style lang="stylus" scoped>
-.echarts 
-  width 100%
-  height 420px
+.echarts {
+  width: 100%;
+  height: 420px;
+}
 </style>
